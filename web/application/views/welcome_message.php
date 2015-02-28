9fcf6532394c2a6a44d6da228200b6daa123d987
sdarmaputra <surya.igede@gmail.com>
2015-02-28 21:08:46 +0700

    perubahan sedikti di welcome page

--- web/application/views/welcome_message.php
+++ web/application/views/welcome_message.php
@@ -70,15 +70,11 @@
 	<h1>Welcome to CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Aplikasi belum lengkap.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+		<p>Masuk ke dasbor melalui link berikut:</p>
+		<code><a href="<?php echo site_url('dasbor'); ?>">Dasbor</a></code>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
-
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
