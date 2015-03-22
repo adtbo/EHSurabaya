5d4799056d49c91d86b4a474686aa3ea8edd833d
aryashinji <ashura.shinji@gmail.com>
2015-03-23 01:57:09 +0800

    Update model
    aku udah push tadi, tpi ternyata enggak
    males jelasin lagi. capek

--- web/application/models/event_model.php
+++ web/application/models/event_model.php
@@ -4,14 +4,26 @@
 	*/
 	class Event_model extends CI_Model
 	{
-
+	
 		function __construct()
 		{
 			parent::__construct();
 			$this->load->database();
 		}
+		
+		public function getID()
+		{
+			$i = 0;
+			$save = $this->db->query('select IDEvent from Event where IDEvent ="'.$i.'"');
+			while($save->num_rows() == 1)
+			{
+				$i = $i + 1;
+				$save = $this->db->query('select IDEvent from Event where IDEvent ="'.$i.'"');
+			}
+			return $i;
+		}
 
-		public function getAll()
+		public function find()
 		{
 			$query = $this->db->get('event');
 			return $query->result(); 
@@ -25,7 +37,8 @@
 
 		public function masuk($data)
 		{
-			return $this->db->insert('event',$data);
+			$query = $this->db->insert('event',$data);
+			return $query;
 		}
 
 		public function update($id, $data)
