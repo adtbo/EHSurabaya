b87a408a42690a53e9d2ba56ab14d2319fa312b1
agus4171 <agus.4171.wirawan@gmail.com>
2015-03-23 16:53:42 +0700

    update frontend

--- web/application/controllers/website.php
+++ web/application/controllers/website.php
@@ -3,6 +3,26 @@
 class Website extends CI_Controller {
 	public function index()
 	{
+		#ambil deskripsi oorgansasi
+		$this->load->model('organisasi_model');
+        $query = $this->organisasi_model->getAll(1);
+        foreach ($query as $row)
+        {
+            $data['deskripsi_organisasi'] = $row->Deskripsi;
+        }
+
+        #ambil data event
+        $this->load->model('event_model');
+        $query = $this->event_model->getAll();
+        $i = 0;
+        foreach ($query as $row)
+        {
+            $data['kegiatan'][$i]['id'] = $row->IDEvent;
+            $data['kegiatan'][$i]['nama'] = $row->NamaEvent;
+            $data['kegiatan'][$i]['deskripsi'] = $row->DeskripsiEvent;
+            $i++;
+        }
+		
 		$this->load->view('website/v_websitehead');
 		$this->load->view('index');
 		$this->load->view('website/v_websitefoot');
